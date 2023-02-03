7bdcb1ef76a33a537dddfa062e21b0ae9a870bd1
pk8101 <pradeepvirat913@gmail.com>
2023-02-03 23:16:08 +0530

    props added

--- src/Cart.js
+++ src/Cart.js
@@ -2,19 +2,52 @@ import React from "react";
 import CartItem from "./CartItem";
 class Cart extends React.Component
 {
+    constructor()
+    {
+        super();
+        this.statis={
+            products:[{
+                price:9999,
+                title:'phone',
+                qty: 1,
+                img:'',
+                id:1
+            },
+            {
+                price:99,
+                title:'earphones',
+                qty: 1,
+                img:'',
+                id:2
+            },
+            {
+                price:990,
+                title:'powerbank',
+                qty: 1,
+                img:'',
+                id:3
+            }
+
+            ],
+            
+        }
+    }
+    
        render()
        {
+        const{products}=this.statis;
         return(
-            <div className="cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-                <CartItem />
-            </div>
             
-       
+            <div className="cart">
+              {
+                products.map((product) => {
+                    return <CartItem product={product} 
+                                     key={product.id}/>;
+                    
+                })
+                
+            }
+            </div>
         )
         }       
 }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,45 +1,21 @@
 import React from "react";
 class CartItem extends React.Component
 {
-    constructor()
-    {
-        super();
-        this.statis={
-            price:999,
-            title:'phone',
-            qty: 1,
-            img:''
-        }
-    }
-    increaseQty=()=> {
-        console.log('this.state',this.statis);
-        //this.statis.qty=this.statis.qty;
-        this.statis.qty++;
-    }
-    decreaseQty=()=> {
-        console.log('this.state',this.statis);
-        //this.statis.qty=this.statis.qty;
-        var x=this.statis.qty;
-        x--;
-        this.statis.qty=x;
-
-    }
-    deleteQty=()=> {
-        console.log('this.state',this.statis);
-        // this.statis.qty=this.statis.qty;
-        this.statis.qty=0;
-    }
+   
+  
     render()
     {
+        console.log('this.prop',this.props)
+        const{price,title,qty,img}=this.props.product;
         return(
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div  style={{fontSize:25}}>{this.statis.title}</div>
-                    <div style={{color:"grey"}}>{this.statis.price}</div>
-                    <div style={{color:"grey"}}>{this.statis.qty}</div>
+                    <div  style={{fontSize:25}}>{title}</div>
+                    <div style={{color:"grey"}}>{price}</div>
+                    <div style={{color:"grey"}}>{qty}</div>
                     <div className="cart-item-action">
                         <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png" onClick={this.increaseQty}/>
                         <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" onClick={this.decreaseQty}/>
